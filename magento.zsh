97d5e3ab14c58aa67050177cb49713c6ce371385
Sam Tay <sam.chong.tay@gmail.com>
2015-08-18 11:07:47 -0400

    update clear-* commands to register webroot

--- magento.zsh
+++ magento.zsh
@@ -1,10 +1,22 @@
-alias fix-permissions="sudo chmod -R 777 media var app/etc && git config core.fileMode false"
-alias clear-cache-rm="sudo rm -rf var/cache var/full_page_cache"
+function clear-cache-rm() {
+  if [ -d webroot ]; then
+    sudo rm -rf webroot/var/cache webroot/var/full_page_cache
+  else
+    sudo rm -rf var/cache var/full_page_cache
+  fi
+}
+function clear-session() {
+  if [ -d webroot ]; then
+    sudo rm -rf webroot/var/session
+  else
+    sudo rm -rf var/session
+  fi
+}
 alias clear-cache-n98="n98-magerun.phar cache:flush && n98-magerun.phar cache:clean"
-alias clear-session="sudo rm -rf var/session"
+alias reindex-site="n98-magerun.phar index:reindex:all"
+alias fix-permissions="sudo chmod -R 777 media var app/etc && git config core.fileMode false"
 alias watch-exception="less +F var/log/exception.log"
 alias watch-system="less +F var/log/system.log"
-alias reindex-site="n98-magerun.phar index:reindex:all"
 
 function sanitize() {
   BASE_URL="http://$1/";
