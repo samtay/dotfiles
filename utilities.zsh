574185f51ac9f3d1413fa833ac3fc418d94c07c9
Sam Tay <sam.chong.tay@gmail.com>
2015-09-23 13:36:17 -0400

    add copy file utility

--- utilities.zsh
+++ utilities.zsh
@@ -1,6 +1,9 @@
 alias g2sites='cd /Volumes/Sites && ll'
 alias copy-ssh-key='cat ~/.ssh/id_rsa.pub | pbcopy'
 alias reload-zsh='source ~/.zshrc'
+copy-file(){
+  cat $1 | pbcopy
+}
 watch(){
   less +F $1
 }
