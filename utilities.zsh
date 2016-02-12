200104a4bae5b85766c854ef9e625bf216fd3e83
Sam Tay <sam.chong.tay@gmail.com>
2016-02-12 18:52:23 -0500

    Remove mac stuff, add todos

--- utilities.zsh
+++ utilities.zsh
@@ -1,21 +1,16 @@
-alias g2sites='cd /Volumes/Sites && ll'
+alias g2sites='cd /home/sites && ll'
 alias g2modules='cd ~/modules'
 alias g2innovation='cd ~/innovation'
-alias copy-ssh-key='cat ~/.ssh/id_rsa.pub | pbcopy'
+alias copy-ssh-key='echo "todo:: fix this"'
 alias reload-zsh='source ~/.zshrc'
 alias ba-jump="ssh -o 'ProxyCommand ssh -W %h:%p jump'"
 alias ba-jump-scp="scp -o 'ProxyCommand ssh -W %h:%p jump'"
 copy-file(){
-  cat $1 | pbcopy
+echo "todo:: fix this"
 }
 watch(){
   less +F $1
 }
-
-UNCHY_DIR=$(dirname `gem which lunchy`)/../extras
-if [ -f $LUNCHY_DIR/lunchy-completion.bash ]; then
-  . $LUNCHY_DIR/lunchy-completion.bash
-fi
 
 mysql-command() {
   echo "$1" | mysql -uroot -proot
