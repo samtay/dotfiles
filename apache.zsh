1f7da86441ece104c02542bd43a0ecb27a16e9cc
Sam Tay <sam.chong.tay@gmail.com>
2016-02-12 18:44:40 -0500

    Update apache commands

--- apache.zsh
+++ apache.zsh
@@ -1,31 +1,29 @@
-alias restart-apache='sudo apachectl restart'
+alias restart-apache='sudo service apache2 restart'
 alias watch-apache='less +F /var/log/apache2/error_log'
 
-SITES_DIR=/Volumes/Sites/
+SITES_DIR=$HOME/git/sites
+ZSH_SSL_CERT=$HOME/ssl/server.crt
+ZSH_SSL_KEY=$HOME/ssl/server.key
 
 VHOST_TEMPLATE="
 <VirtualHost *:80>
-  ServerName {temp}.dev
-  DocumentRoot $SITES_DIR{temp}.dev
-</VirtualHost>
-"
-
-VHOST_TEMPLATE_WEBROOT="
-<VirtualHost *:80>
-  ServerName {temp}.dev
-  DocumentRoot $SITES_DIR{temp}.dev/webroot
+  ServerName {site_tolower}.dev
+  DocumentRoot $SITES_DIR/{site_toupper}/webroot
 </VirtualHost>
 "
 
 add-site(){
-  echo "$VHOST_TEMPLATE" | sed "s/{temp}/$1/g" | sudo tee /etc/apache2/extra/vhosts.d/vhost-http-$1.conf > /dev/null
-  echo "127.0.0.1 $1.dev" | sudo tee -a /etc/hosts > /dev/null
-  sudo apachectl restart
+  local site_tolower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+  local site_toupper=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+  echo "$VHOST_TEMPLATE" | sed "s/{site_tolower}/$site_tolower/g" | sed "s/{site_toupper}/$site_toupper/g" | sudo tee /etc/apache2/sites-available/$site_tolower.conf > /dev/null
+  echo "127.0.0.1 $site_tolower.dev" | sudo tee -a /etc/hosts > /dev/null
+  sudo a2ensite $site_tolower.conf
+  sudo service apache2 reload
 }
 
-add-site-with-webroot(){
-  echo "$VHOST_TEMPLATE_WEBROOT" | sed "s/{temp}/$1/g" | sudo tee /etc/apache2/extra/vhosts.d/vhost-http-$1.conf > /dev/null
-  echo "127.0.0.1 $1.dev" | sudo tee -a /etc/hosts > /dev/null
-  sudo apachectl restart
+remove-site(){
+  local site_tolower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+  sudo a2dissite $site_tolower
+  sudo rm /etc/apache2/sites-available/$site_tolower.conf
+  sudo service apache2 reload
 }
-
