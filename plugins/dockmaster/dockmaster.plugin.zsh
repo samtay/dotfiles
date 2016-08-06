9ffb99b940c1df2a11475614da7d882a3309b7ae
Sam Tay <sam.chong.tay@gmail.com>
2016-08-06 11:04:22 -0400

    Only show autocomplete for first arg

--- plugins/dockmaster/dockmaster.plugin.zsh
+++ plugins/dockmaster/dockmaster.plugin.zsh
@@ -1,9 +1,6 @@
 #compdef dockmaster
 
-_list_compositions () {
-  if ! _is_in_docker_repo; then
-    return "";
-  fi
+_list_compositions() {
   local compositions_dir="$(_get_repo_root)/compositions"
   local escaped_compositions_dir=$(echo $compositions_dir | sed -e "s/\//\\\\\//g")
   find $compositions_dir -maxdepth 1 -mindepth 1 \
@@ -11,9 +8,16 @@ _list_compositions () {
     | sed -e "s/\///g"
 }
 
+_compadd_compositions () {
+  compadd $(_list_compositions)
+}
+
 _dockmaster () {
-  compadd `_list_compositions`
+  if ! _is_in_docker_repo; then
+    return 1
+  fi
+  _arguments -s \
+    '1: :_compadd_compositions' \
 }
 
 compdef _dockmaster dockmaster
-
