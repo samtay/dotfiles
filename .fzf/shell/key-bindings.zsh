35e8306d89656af1a3dd2096443f1b0e6167ce4b
Sam Tay <samctay@pm.me>
2022-10-13 10:50:32 -0700

    Git stuff

--- .fzf/shell/key-bindings.zsh
+++ .fzf/shell/key-bindings.zsh
@@ -70,7 +70,7 @@ bindkey -M emacs '^T' fzf-file-widget
 bindkey -M vicmd '^T' fzf-file-widget
 bindkey -M viins '^T' fzf-file-widget
 
-# ALT-C - cd into the selected directory
+# CTRL-G - cd into the selected directory
 fzf-cd-widget() {
   local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
     -o -type d -print 2> /dev/null | cut -b3-"}"
@@ -89,9 +89,9 @@ fzf-cd-widget() {
   return $ret
 }
 zle     -N             fzf-cd-widget
-bindkey -M emacs '\ec' fzf-cd-widget
-bindkey -M vicmd '\ec' fzf-cd-widget
-bindkey -M viins '\ec' fzf-cd-widget
+bindkey -M emacs '^G' fzf-cd-widget
+bindkey -M vicmd '^G' fzf-cd-widget
+bindkey -M viins '^G' fzf-cd-widget
 
 # CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
